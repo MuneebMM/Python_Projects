498610f5cf1c24c8553eb75aeae7ae164ddc34e0
MuneebMM <muneebmm007@gmail.com>
2023-09-26 12:58:24 +0530

    Fifth

---
+++ PasswordGenerator.py
@@ -0,0 +1,23 @@
+import random
+
+Letters = ['A','B','C','D','E','F','G','H','I','J'
+           ,'K','L','M','N','O','P','Q','R','S','T'
+           ,'U','V','W','X','Y','Z']
+
+numbers=['1','2','3','4','5','6','7','8','9']
+symbols = ['!','@','#','$','%','^','&','*','(',')'
+           ,'_','+','=']
+
+print("Welcome to my password generator!")
+nr_letters = int(input("How many Letters would like to generate in password\n"))
+nr_symbols = int(input("How many symbols would you like to generate in the password\n"))
+nr_numbers = int(input("How many numbers would you like to generate in the password\n"))
+
+password = ""
+for char in range(1,nr_letters+1):
+   password += random.choice(Letters)
+for char in range(1,nr_symbols+1):
+    password += random.choice(symbols)
+for char in range(2,nr_numbers+1):
+    password += random.choice(numbers)
+print("Password is: ", password)
